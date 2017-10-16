aa3f4c5bba9dd5aff79dfd38615de6be496f69fd
kaushal-dev <kaushal-dev@outlook.com>
2017-10-16 21:46:07 +0100

    Spring MVC framework
    Added home page with spring mvc wiring

---
+++ src/main/java/com/ratingservice/web/config/WebAppInitializer.java
@@ -0,0 +1,41 @@
+package com.ratingservice.web.config;
+
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    /* implements WebApplicationInitializer
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.setConfigLocation("com.ratingservice.web.config.WebConfig");
+        ctx.setServletContext(servletContext);
+
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/");
+    }*/
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { WebConfig.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+}
